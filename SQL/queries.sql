eebf03604e04d655ecba49027cef0f9bda7c4e45
Peter Zhang <126302092+zhangp124@users.noreply.github.com>
2026-05-01 01:18:44 -0400

    Update queries.sql

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,1 +1,15 @@
+--Player Performance Query
+SELECT PlayerID, AVG(Points) AS AvgPoints
+FROM PlayerStatistics
+GROUP BY PlayerID;
 
+--Team Comparison
+SELECT TeamID, AVG(Points) AS AvgPoints
+FROM TeamStatistics
+GROUP BY TeamID
+ORDER BY AvgPoints DESC;
+
+--Game Analysis
+SELECT g.GameID, t.TeamID, t.Points
+FROM Games g
+JOIN TeamStatistics t ON g.GameID = t.GameID;
