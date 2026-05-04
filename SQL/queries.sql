4201c94e81a4b8581527937e23253ea7beedda60
Peter Zhang <126302092+zhangp124@users.noreply.github.com>
2026-05-03 21:22:42 -0400

    Update queries.sql

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -6,17 +6,46 @@ ON ps.PersonID = p.PersonID
 WHERE p.PersonID IS NULL;
 
 --Player Performance Query
-SELECT PlayerID, AVG(Points) AS AvgPoints
-FROM PlayerStatistics
-GROUP BY PlayerID;
+SELECT 
+p.FirstName,
+p.LastName,
+ROUND(AVG(ps.Points), 2) AS AvgPoints
+FROM PlayerStatistics AS ps
+INNER JOIN Players AS p
+ON ps.PersonID = p.PersonID
+GROUP BY 
+p.FirstName,
+p.LastName;
 
 --Team Comparison
-SELECT TeamID, AVG(Points) AS AvgPoints
+SELECT 
+TeamName,
+ROUND(AVG(TeamScore), 2) AS AvgPoints
 FROM TeamStatistics
-GROUP BY TeamID
-ORDER BY AvgPoints DESC;
+GROUP BY TeamName
+ORDER BY ROUND(AVG(teamScore), 2) DESC;
 
 --Game Analysis
-SELECT g.GameID, t.TeamID, t.Points
-FROM Games g
-JOIN TeamStatistics t ON g.GameID = t.GameID;
+SELECT 
+g.GameID,
+
+g.hometeamName AS Home,
+g.homeScore AS HomeScore,
+t1.FieldGoalsMade AS HomeFGM,
+t1.FieldGoalsAttempted AS HomeFGA,
+t1.FieldGoalsPercentage AS HomeFGPer,
+
+g.awayteamName AS Away,
+g.awayScore AS AwayScore,
+t2.FieldGoalsMade AS AwayFGM,
+t2.FieldGoalsAttempted AS AwayFGA,
+t2.FieldGoalsPercentage AS AwayFGPer
+
+FROM (Games AS g
+INNER JOIN TeamStatistics AS t1
+ON g.GameID = t1.GameID)
+INNER JOIN Teamstatistics AS t2
+ON g.GameID = t2.GameID
+
+WHERE t1.teamName = g.hometeamName
+AND t2.opponentteamName = g.awayteamName;
